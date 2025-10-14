efcc114349bf8d0d59ba6cdb17f5c070747258d3
Diego <diegogonzalezsoto1@gmail.com>
2025-10-14 16:45:44 +0200

    a

--- DWCS/UD2/Actividade 2.4/1.php
+++ DWCS/UD2/Actividade 2.4/1.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="Actividade 2.4/css">
 </head>
 <body>
     <h1>Tienda de ropa</h1>
 
     <form action = "" method="get">
         <div>
-            <lablel for= "prenda"></lablel>
-            <select name="prenda" id = "prenda" multiple>
+            <lablel for= "koprenda" class="form-label"></lablel>
+            <select name="prenda" id = "prenda" multiple class="form-select">
                 <option value="value1">Abrigos</option>
                 <option value="value2">Tops</option>
                 <option value="value3">Camisas</option>
@@ -21,8 +21,8 @@
         </div>
 
         <div>
-            <div><label for = "color">color</label></div>
-            <input type="color" name="color">
+            <div><label class="form-label" for = "color">color</label></div>
+            <input type="color" name="color" class="form-control form-color">
         </div>
 
         <div><input type="submit"></div>
