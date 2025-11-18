04d2459b996a2a337c6a2cdaa0cb1de4f31c7ce7
Diego <diegognzalezsoto1@gmail.com>
2025-11-18 17:13:27 +0100

    Fix HTML structure and improve form element attributes in 1.php

--- DWCS/UD2/Actividade 2.4/1.php
+++ DWCS/UD2/Actividade 2.4/1.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,13 +8,14 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="Actividade 2.4/css">
 </head>
+
 <body>
     <h1>Tienda de ropa</h1>
 
-    <form action = "" method="get">
+    <form action="" method="get">
         <div>
-            <lablel for= "koprenda" class="form-label"></lablel>
-            <select name="prenda" id = "prenda" multiple class="form-select">
+            <lablel for="koprenda" class="form-label"></lablel>
+            <select name="prenda" id="prenda" multiple class="form-select">
                 <option value="value1">Abrigos</option>
                 <option value="value2">Tops</option>
                 <option value="value3">Camisas</option>
@@ -21,37 +23,38 @@
         </div>
 
         <div>
-            <div><label class="form-label" for = "color">color</label></div>
+            <div><label class="form-label" for="color">color</label></div>
             <input type="color" name="color" class="form-control form-color">
         </div>
 
         <div><input type="submit"></div>
     </form>
 
-    <?php 
+    <?php
     //cambiar $_POST por $_GET en función del método HTTP utilizado
     foreach ($_GET as $clave => $valor) {
 
-    echo "<strong>$clave</strong>:";
+        echo "<strong>$clave</strong>:";
 
         if (!is_array($valor)) {
-            if ($clave =="color"){
+            if ($clave == "color") {
                 $color = $valor;
 
                 echo "<span style=\"background-color:$color\">$valor</span>";
-            }else{
+            } else {
                 echo " $valor";
             }
         } else {
             echo "<pre>";
-            print_r ($valor);
+            print_r($valor);
             echo "</pre>";
         }
 
         echo "<br/>";
     }
 
-    
+
     ?>
 </body>
+
 </html>
