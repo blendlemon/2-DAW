5aef1e96aa08f1a2806059a9c39b83047110bc2a
Diego <diegognzalezsoto1@gmail.com>
2025-11-19 02:35:09 +0100

    Add initial HTML, CSS, and JavaScript for slideshow feature with audio playback
    - Created html.html with structure for slideshow and audio controls.
    - Implemented script.js to handle slide navigation and audio playback on scroll.
    - Developed style.css for styling the slideshow, including background images and animations.
    - Enhanced user interaction with clickable dots for slide selection and audio feedback.

---
+++ DIW/UD2/menu bueno/original/script.js
@@ -0,0 +1,45 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+window.addEventListener('wheel', function (event) {
+    if (event.deltaY > 0) {
+        plusSlides(1);
+        playAudio('https://tim-school.com/codepen/clown/slide.mp3');
+    } else {
+        plusSlides(-1);
+        playAudio('https://tim-school.com/codepen/clown/slide.mp3');
+    }
+});
+
+var audio_;
+
+function playAudio(src) {
+    if (audio_) audio_.pause();
+    const audio = audio_ = new Audio(src);
+    audio.play();
+    audio.currentTime = 0;
+}
