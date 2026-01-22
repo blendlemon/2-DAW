c42acd66c2ecc59574bc108f3d0d3640d11ffea6
Diego <diegogonzalezsoto1@gmail.com>
2026-01-22 20:37:53 +0100

    Refactor HTML structure and styles; add JavaScript functions for day and search logic

--- DWCC/UD4/Ejercicios/EjerciciosUD4_2/Ejercicio1/script.js
+++ DWCC/UD4/Ejercicios/EjerciciosUD4_2/Ejercicio1/script.js
@@ -1,3 +1,6 @@
-function diaSemana(n1, n2) {
-    let dias = "Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo".split(,)
+function diaSemana(n1, n2 = 0) {
+    n1 -= 1;
+    let dias = "Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo".split(",");
+    dias[-1] = "Domingo";
+    return dias[n1 - n2];
 }
