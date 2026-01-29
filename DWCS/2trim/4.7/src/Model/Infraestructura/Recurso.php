bb2efb58aa6ac5d0e3c280295f3e1d76f13fa3b5
Diego <diegogonzalezsoto1@gmail.com>
2026-01-29 17:39:01 +0100

    add getter method for descripcion in Recurso class

--- DWCS/2trim/4.7/src/Model/Infraestructura/Recurso.php
+++ DWCS/2trim/4.7/src/Model/Infraestructura/Recurso.php
@@ -7,4 +7,14 @@ class Recurso{
     public function __construct(string $descripcion) {
         $this->descripcion = $descripcion;
     }
+
+    /**
+     * Get the value of descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion(): string
+    {
+        return $this->descripcion;
+    }
 }
