9261260df8a07d7e6d29fc75465c8bf5904da59e
Diego <diegogonzalezsoto1@gmail.com>
2026-01-30 19:16:33 +0100

    refactor Logger trait to create log directory if it doesn't exist

--- DWCS/2trim/4.7/src/Service/Traits/Logger.php
+++ DWCS/2trim/4.7/src/Service/Traits/Logger.php
@@ -7,7 +7,9 @@ trait Logger {
     public function log(string $mensaje, ?string $nivel = null, ?string $fileName=null): void {
         $nivel = $nivel?? $this->nivelLog;
         $fileName = $fileName?? $this->fileName;
-      //escribe en un fichero app.log en el directorio actual
-        error_log("[$nivel] " . date('Y-m-d H:i:s') . " - " . $mensaje . "\n", 3, \dirname(__DIR__, 3 ) . \DIRECTORY_SEPARATOR . "log". \DIRECTORY_SEPARATOR . $fileName);
-    }
+      //comprueba si el directorio log existe si no lo crea, despues escribe los logs pertinentes en el
+      if (!\is_dir(\dirname(__DIR__,3)). \DIRECTORY_SEPARATOR . 'log') {
+        mkdir(\dirname(__DIR__,3) . \DIRECTORY_SEPARATOR . 'log', 0777, true);
+      }
+        error_log("[$nivel] " . date('Y-m-d H:i:s') . " - " . $mensaje . "\n", 3, \dirname(__DIR__, 3) . \DIRECTORY_SEPARATOR . 'log' .  \DIRECTORY_SEPARATOR . $fileName);
 }
