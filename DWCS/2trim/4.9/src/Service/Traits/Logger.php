4e8a1fadec0cd20c1f5e615d53e4cd65b2ee519f
Diego <diegogonzalezsoto1@gmail.com>
2026-01-30 17:01:37 +0100

    4.8(4.7 autoload) y 4.9

---
+++ DWCS/2trim/4.9/src/Service/Traits/Logger.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Service\Traits;
+trait Logger {
+    private string $nivelLog = 'INFO';
+    private string $fileName = 'app.log';
+
+    public function log(string $mensaje, ?string $nivel = null, ?string $fileName=null): void {
+        $nivel = $nivel?? $this->nivelLog;
+        $fileName = $fileName?? $this->fileName;
+      //escribe en un fichero app.log en el directorio actual
+        error_log("[$nivel] " . date('Y-m-d H:i:s') . " - " . $mensaje . "\n", 3, \dirname(__DIR__, 3 ) . \DIRECTORY_SEPARATOR . "log". \DIRECTORY_SEPARATOR . $fileName);
+    }
+}
