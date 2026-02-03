397b351b311a2a00a7384714b5cc2ce2d4d2e16e
Diego <diegogonzalezsoto1@gmail.com>
2026-02-03 18:13:22 +0100

    Add initial HTML and JavaScript files for Ejercicio19; implement Plato class and menu display logic

---
+++ DWCC/UD4/Ejercicios/EjerciciosUD4_Objetos_II/script.js
@@ -0,0 +1,57 @@
+class Plato {
+    static id = 0;
+    constructor(nombre, tipo, precio) {
+        this.id = Plato.id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precio = precio;
+        Plato.id++;
+    }
+
+    static menuBarato(platos) {
+        let platostipo = {};
+        let preciototal = 0;
+        let resultado = '<h1><b>Menú</b></h1>';
+        for (let i = 0; i < platos.length; i++) {
+            if (!platostipo[platos[i].tipo]) {
+                platostipo[platos[i].tipo] = [];
+            }
+            platostipo[platos[i].tipo].push(platos[i]);
+        }
+
+        for (let i in platostipo) {
+            platostipo[i].sort((a, b) => a.precio - b.precio);
+            if (platostipo[i].length > 0) {
+                resultado += `${platostipo[i][0].tipo} : ${platostipo[i][0].nombre}<br>`;
+                preciototal += platostipo[i][0].precio;
+            }
+        }
+        resultado += `Precio: ${preciototal}€`
+        return resultado;
+    }
+
+    static menusUmbral(platos) {
+        let platostipo = {};
+        let precioMaximo = 0;
+        let resultado = '<h1><b>Menú</b></h1>';
+        for (let i = 0; i < platos.length; i++) {
+            if (!platostipo[platos[i].tipo]) {
+                platostipo[platos[i].tipo] = [];
+            }
+            platostipo[platos[i].tipo].push(platos[i]);
+        }
+
+        for (let i in platostipo) {
+            platostipo[i].sort((a, b) => b.precio - a.precio);
+            if (platostipo[i].length > 0) {
+                precioMaximo += platostipo[i][0].precio;
+            }
+        }
+
+        for (let i in platostipo) {
+            for
+        }
+        resultado += `Precio: ${preciototal}€`
+        return resultado;
+    }
+}
