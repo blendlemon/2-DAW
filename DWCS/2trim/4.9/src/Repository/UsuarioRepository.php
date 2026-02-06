cd89d87da93c55fde82f15ee56d8a6c78082ddd2
Diego <diegogonzalezsoto1@gmail.com>
2026-02-06 18:28:39 +0100

    Add AbstractRepository and UsuarioRepository classes for database interaction; implement methods for retrieving all users and creating new users.

---
+++ DWCS/2trim/4.9/src/Repository/UsuarioRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repository;
+
+use PDO;
+use App\Model\Biblioteca\Usuario;
+use App\Repository\AbstractRepository;
+
+class UsuarioRepository extends AbstractRepository
+{
+    protected string $table = 'usuarios';
+
+    public function findByEmail(string $email): ?array
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM usuarios WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC) ?: null;
+    }
+
+
+    public function create(Usuario $usuario): ?object
+    {
+        $stmt = $this->pdo->prepare(
+            "INSERT INTO usuarios (nombre, email) VALUES (?, ?)"
+        );
+        $stmt->execute([
+            $usuario->getNombre(),
+            $usuario->getEmail()
+        ]);
+
+        //Habría que añadir a Usuario el atributo id nullable y su getter y setter para que esto funcione
+        $id = $this->pdo->lastInsertId();
+        if ($id === false) {
+            return null;
+        }
+        $usuario->setId($id);
+        return $usuario;
+    }
+}
