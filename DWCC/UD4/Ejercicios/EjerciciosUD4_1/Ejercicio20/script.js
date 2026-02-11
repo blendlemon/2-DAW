412fe7c26762fed06c1cd2291e4da8484bc24d23
Diego <diegogonzalezsoto1@gmail.com>
2026-02-11 12:22:29 +0100

    Refactor dame_numero_casillas_blanco function to simplify movement calculations and initialize nMovimientos correctly

--- DWCC/UD4/Ejercicios/EjerciciosUD4_1/Ejercicio20/script.js
+++ DWCC/UD4/Ejercicios/EjerciciosUD4_1/Ejercicio20/script.js
@@ -29,32 +29,26 @@ function crearTablero() {
 }
 
 function dame_numero_casillas_blanco(Tablero) {
-    let nMovimientos = 8;
+    let nMovimientos = 0;
 
     Tablero.find(x => x.fila == 0 && x.columna == 0).pieza = 1000;
     Tablero.find(x => x.fila == 0 && x.columna == 1).pieza = 10;
 
     let rey = Tablero.find(rey => rey.pieza == 1000);
-    
-    // por posicion es mas facil y simple de hacer los calculos dependen de si esta en 0 o 7 si esta en cero se le suma uno para comprobar
-    // si esta en 7 se le resta simplifica todos los calculos de tener que hacer 4 sets de 3 if a 1 y solo 2 para establecer las variables
-    //que tambien va a ayudar con todos los calculos dependientes de los limites del tablero
 
-    if ((rey.fila == 0 && rey.columna == 0) || (rey.fila == 7 && rey.columna == 7) || (rey.fila == 0 && rey.columna == 7) || (rey.fila == 7 && rey.columna == 0)) {
-        nMovimientos -= 5;
-        if (rey.fila == 0 && rey.columna == 0) {
-            if (Tablero.find(pos => pos.fila == rey.fila + 1 && pos.columna == rey.columna).pieza!=null) {
-                nMovimientos -= 1;
-            }
-            if (Tablero.find(pos => pos.fila == rey.fila + 1 && pos.columna == rey.columna + 1).pieza!=null) {
-                nMovimientos -= 1;
-            }
-            if (Tablero.find(pos => pos.fila == rey.fila  && pos.columna == rey.columna + 1).pieza!=null) {
-                nMovimientos -= 1;
+    for (let i = rey.fila - 1; i <= rey.fila + 1; i++) {
+        if (i >= 0 && i <= 7) {
+            for (let j = rey.columna - 1; j <= rey.columna + 1; j++) {
+                if (j >= 0 && j <= 7) {
+                    if (i != rey.fila || j != rey.columna) {
+                        if (Tablero.find(x => x.fila == i && x.columna == j).pieza === null) {
+                            nMovimientos += 1;
+                        }
+                    }
+                }
             }
         }
     }
-
 
     return nMovimientos;
 
