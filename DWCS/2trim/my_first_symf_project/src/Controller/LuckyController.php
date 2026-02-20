107b8f36b60767fb8c46a824af2a83df203fbd1c
Diego <diegogonzalezsoto1@gmail.com>
2026-02-20 18:14:36 +0100

    feat: Add LuckyController with index and number methods, and corresponding template and test

---
+++ DWCS/2trim/my_first_symf_project/src/Controller/LuckyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LuckyController extends AbstractController
+{
+    #[Route('/lucky/number/{max}', name: 'app_lucky')]
+
+    public function number(int $max){
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '. $number.'</body></html>'
+        );
+    }
+    public function index(): Response
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+}
