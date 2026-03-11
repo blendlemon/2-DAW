f28a16b2a830fa61a907c7383b088b3d01498ee7
Diego <diegogonzalezsoto1@gmail.com>
2026-03-11 17:14:35 +0100

    Add repositories, templates, and tests for Editorial, Libro, and Nota entities
    - Created EditorialRepository, LibroRepository, and NotaRepository for managing database interactions.
    - Added templates for displaying and managing books and notes, including search and listing functionalities.
    - Implemented tests for LibroController and NotaController to ensure successful responses.
    - Introduced a base template for consistent layout across pages.
    - Added a .gitignore file for translations and bootstrap file for test environment setup.

---
+++ DWCS/2trim/r26_Actividad4.16-sol/src/Controller/LibroController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LibroRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LibroController extends AbstractController
+{
+   
+
+    #[Route('/libros/buscar', name: 'libro_buscar')]
+    public function buscar(Request $request, LibroRepository $libroRepository): Response
+    {
+        // Obtenemos los parámetros de la query
+        $titulo   = $request->query->get('titulo');   // devuelve string o null
+        $unidades = $request->query->get('unidades'); // siempre string si se pasa
+        $autor    = $request->query->get('autor');    // string o null
+
+        // Manejar cadenas vacías o parámetros no enviados
+        $titulo   = !empty($titulo) ? $titulo : null;
+        $unidades = !empty($unidades) ? (int) $unidades : null; // convertimos a int
+        $autor    = !empty($autor) ? $autor : null;
+
+        // Ahora podemos usar estos valores para buscar en el repositorio
+      
+        $libros = $libroRepository->buscarLibros($titulo, $unidades, $autor);
+
+        return $this->render("libro/busqueda.html.twig", ["libros" => $libros]);
+    }
+}
